c2cce975fd1e7b7186d4dd4b0d03200f9c00052a
sivaneshrusselraj <75260690+sivaneshrusselraj@users.noreply.github.com>
2020-12-04 16:55:17 +0530

    Add files via upload

---
+++ removeObject.js
@@ -0,0 +1,5 @@
+Array.prototype.remove = function removeObject(){
+    Object.remove({'address':'ABC'});
+}
+var obj = [{'id':5,'age':33,'address':'ABC'},{'id':'6','age':'32','address':'ABC'}];
+return removeObject();
